c3ebffc3b391c5cb02d3a669870fecc04820a305
Konstantin S <badmantegrow1@gmail.com>
2022-08-08 00:48:32 +0300

    Пятый элемент версия 3

--- 02_groundhog_day.py
+++ 02_groundhog_day.py
@@ -28,48 +28,52 @@ class SuicideError(Exception):
 
 def one_day():
     enlightenment_carma_level = 0
+    with open('karma.log', 'w'):
+        pass
     while enlightenment_carma_level < 777:
         dice = random.randint(1, 13)
-        try:
-            if dice == 1:
-                enlightenment_carma_level += 1
-                raise IamGodError('Я Бог')
-        except IamGodError as exc:
-            print(f'Поймано исключение {exc}')
-        try:
-            if dice == 2:
-                enlightenment_carma_level += 2
-                raise DrunkError('Напился')
-        except DrunkError as exc:
-            print(f'Поймано исключение {exc}')
-        try:
-            if dice == 3:
-                enlightenment_carma_level += 3
-                raise CarCrashError('Разбился на машине')
-        except CarCrashError as exc:
-            print(f'Поймано исключение {exc}')
-        try:
-            if dice == 4:
-                enlightenment_carma_level += 4
-                raise GluttonyError('Обожрался')
-        except GluttonyError as exc:
-            print(f'Поймано исключение {exc}')
-        try:
-            if dice == 5:
-                enlightenment_carma_level += 5
-                raise DepressionError('Депресняк')
-        except DepressionError as exc:
-            print(f'Поймано исключение {exc}')
-        try:
-            if dice == 6:
-                enlightenment_carma_level += 6
-                raise SuicideError('Суицид')
-        except SuicideError as exc:
-            print(f'Поймано исключение {exc}')
-        else:
-            if dice > 6:
-                enlightenment_carma_level += 7
-                print(dice)
+        with open('karma.log', 'a+', encoding='utf-8') as karma:
+            try:
+                if dice == 1:
+                    enlightenment_carma_level += 1
+                    raise IamGodError('IamGodError')
+            except IamGodError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            try:
+                if dice == 2:
+                    enlightenment_carma_level += 2
+                    raise DrunkError('DrunkError')
+            except DrunkError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            try:
+                if dice == 3:
+                    enlightenment_carma_level += 3
+                    raise CarCrashError('CarCrashError')
+            except CarCrashError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            try:
+                if dice == 4:
+                    enlightenment_carma_level += 4
+                    raise GluttonyError('GluttonyError')
+            except GluttonyError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            try:
+                if dice == 5:
+                    enlightenment_carma_level += 5
+                    raise DepressionError('DepressionError')
+            except DepressionError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            try:
+                if dice == 6:
+                    enlightenment_carma_level += 6
+                    raise SuicideError('SuicideError')
+            except SuicideError as exc:
+                karma.write(f'Поймано исключение {exc}\n')
+            else:
+                if dice > 6:
+                    enlightenment_carma_level += 7
+                    print(dice)
+            karma.close()
 
 
 one_day()
